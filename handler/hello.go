6452d2629c2f292bdd2974535c2b01aea4a3bece
AlKoFDC <alexander.kogan@freiheit.com>
2016-06-10 17:27:55 +0200

    CHANGED: Responding to the same channel with a mention of the user, that wrote, when receiving a hello message.

--- handler/hello.go
+++ handler/hello.go
@@ -1,12 +1,12 @@
 package handler
 
 import (
-	"fmt"
-	"github.com/AlKoFDC/eva.support/logger"
 	"github.com/AlKoFDC/eva.support/message"
 )
 
 // hello reacts to a message, that greets the bot.
 func (h *SlackMessageHandler) hello(msg message.M) {
-	logger.Standard.Println(fmt.Sprintf("I received a greeting from %s! *blush*", msg.User))
+	// Respond on the same channel.
+	msg.Text = "Hello, <@" + msg.User + ">. How can I help you? :blush:"
+	h.Send(msg)
 }
